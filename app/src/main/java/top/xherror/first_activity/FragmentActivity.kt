b998e1d87606ed7ff25de348270655dab63e4594
xherror@win <gxherror@gmail.com>
2022-09-28 10:04:04 +0800

    week2: begin fragment

--- app/src/main/java/top/xherror/first_activity/FragmentActivity.kt
+++ app/src/main/java/top/xherror/first_activity/FragmentActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import top.xherror.first_activity.databinding.ActivityFragmentBinding
 
@@ -12,6 +13,10 @@ class FragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding =ActivityFragmentBinding.inflate(layoutInflater)
+        val button: Button=findViewById(R.id.fragmentLeftButton)
+        button.setOnClickListener {
+
+        }
         setContentView(binding.root)
     }
 
